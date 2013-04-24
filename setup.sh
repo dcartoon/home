23e234f43aa52ac7ff9b114092dc3e641ced8b27
Dan Cartoon <dcartoon@gmail.com>
2013-04-23 17:11:06 -0700

    Additional setup goodies

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
-#Vim setup
+# Vim setup
 ln -s ~/home/.vimrc ~/.vimrc
 ln -s ~/home/.vim ~/.vim
 
-#Zsh setup
+# Zsh setup
 ln -s ~/home/.zshrc ~/.zshrc
+
+git submodule init
+git submodule update
+
+# Git setup
+echo "Don't forget to setup your Git config"
+echo 'git config --global user.name "John Doe"'
+echo 'git config --global user.email johndoe@example.com'
