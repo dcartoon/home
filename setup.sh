b4f6f3aaea7680a6d893a150df2d880f932f35ce
Dan Cartoon <dcartoon@gmail.oom>
2013-08-21 14:04:16 -0700

    Setup now copies .gitconfig

--- setup.sh
+++ setup.sh
@@ -9,9 +9,9 @@ ln -s ~/home/.zshrc ~/.zshrc
 git submodule init
 git submodule update
 
+cp .gitconfig ~
+
 # Git setup
 echo "Don't forget to setup your Git config"
-echo 'git config --global core.editor vi'
 echo 'git config --global user.name "John Doe"'
 echo 'git config --global user.email johndoe@example.com'
-echo 'git config --global color.ui true'
